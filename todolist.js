8d1e9eb95ac8522560e4b6298771b4f453e44413
viljawallin <wavi22hy@student.ju.se>
2023-04-11 14:21:16 +0200

    Citation

--- todolist.js
+++ todolist.js
@@ -23,7 +23,7 @@ function createTaskElement(task) {
     // When "done" button is clicked - it adds the task to the completedTasks array to add a line-through style to the text. //
     list.style.textDecoration = "line-through";
     completedTasks.push(task);
-    // I used ChatGpt after listening to Garrits lecture about ChatGpt in order to debug my code because at first I could not get my line-through to be saved in localStorage. //
+    // Citation: I used ChatGpt after listening to Garrits lecture about ChatGpt in order to debug my code because at first I could not get my line-through to be saved in localStorage. Accessed April 7, 2023 //
     // My conclusion after using ChatGpt as a tool, was that instead of only applying the line-through decoration when the "done" button is clicked, I wanted to check if the task is already marked as done by checking in an array of completed tasks - stored in localStorage. If it is in the array, then apply the line-through decoration when creating the task element. //
     localStorage.setItem("completedTasks", JSON.stringify(completedTasks));
   };
@@ -32,7 +32,7 @@ function createTaskElement(task) {
   deleteButton.innerText = "Delete";
   deleteButton.className = "deleteButton";
   deleteButton.onclick = () => {
-    // I took this information from hamstergram-workshop-login by Garrit "post.likes.splice(userIndex, 1);" in order to remove one specific task from the task array //
+    // Citation: I took this information from hamstergram-workshop-login by Garrit "post.likes.splice(userIndex, 1);" in order to remove one specific task from the task array. Accessed April 4, 2023 //
     tasks.splice(tasks.indexOf(task), 1);
     localStorage.setItem("tasks", JSON.stringify(tasks));
     tasksContainer.removeChild(list);
@@ -52,7 +52,7 @@ function createTaskElement(task) {
 // Function to display the list of tasks //
 function displayTasks() {
   // Loops throgh the tasks array and adds each task as new HTML element to the createTaskElement function. //
-  // I took information from hamstergram-workshop-login by Garrit - from the function displayPosts - order to make my function displayTasks. The same code but changed it to my variebles and the way I wanted my function to work.//
+  // Citation: I took information from hamstergram-workshop-login by Garrit - from the function displayPosts - order to make my function displayTasks. The same code but changed it to my variebles and the way I wanted my function to work. Accessed April 4, 2023. //
   for (let task of tasks) {
     const list = createTaskElement(task);
     tasksContainer.appendChild(list);
@@ -67,7 +67,7 @@ document.body.appendChild(tasksContainer);
 
 // Event listener to the "add" buton //
 addButton.addEventListener("click", () => {
-  // I took inspiration from https://www.w3schools.com/howto/howto_js_todolist.asp solution when it came to making a new list when clicking the add button in javascript //
+  // Citation: W3Schools How to Create a To-Do List https://www.w3schools.com/howto/howto_js_todolist.asp I accessed it ain April 4, 2023. I used it when it came to get inspiration on how making a new list when clicking the add button in javascript. //
   // Picks up the value of the input field //
   const inputText = document.getElementById("inputField").value;
 
